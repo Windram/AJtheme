9d0904f5fd9d511287653115b9171ebbf7e6f488
Windram <55179556+Windram@users.noreply.github.com>
2021-01-02 18:18:17 +0100

    everything

---
+++ AJTheme/404.php
@@ -0,0 +1,15 @@
+<?php
+    get_header();
+    ?>
+    
+    
+		<article class="content px-3 py-5 p-md-5">
+	   
+        <h1>page Not Found</h1>
+        
+	    </article>
+	    
+    
+<?php
+get_footer();
+?>
---
+++ AJTheme/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer text-right py-2 theme-bg-dark">
+		   
+<?php
+					dynamic_sidebar('footer-1');
+				?>            
+<p class="copyright"><a href="http://alienjefferson.com/">Alien Jefferson</a> | AJTheme by Niels Vind</p>
+            
+        </footer>
+        </div>
+
+<?php
+    wp_footer();
+?>
+
+
+</body>
+</html> 
+
